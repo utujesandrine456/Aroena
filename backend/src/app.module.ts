33f18598c85a325b9176c57ab9ce885a75b13864
utujesandrine456 <utujesandrine456@gmail.com>
2026-04-15 09:04:23 +0200

    solved backend issue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './services/services.module';
@@ -8,8 +9,15 @@ import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ServicesModule, OrdersModule, PrismaModule, UsersModule, AdminModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServicesModule,
+    OrdersModule,
+    PrismaModule,
+    UsersModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
